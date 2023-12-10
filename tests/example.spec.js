b9709dc26b5cf8fdf8809192422203201973b84b
Valentin Nalivayko <valentin.nalivayko@gmail.com>
2023-12-10 23:14:41 +0200

    init commit

---
+++ tests/example.spec.js
@@ -0,0 +1,10 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await response.url('https://playwright.dev/');
+  
+});
+
